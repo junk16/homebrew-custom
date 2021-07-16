8f2c31f9bd4c98b4811cbaef8eeab1b44a8cf944
jun yamada <jun.16@mac.com>
2021-07-16 21:34:58 +0900

    first commit

---
+++ global.rb
@@ -0,0 +1,118 @@
+class Global < Formula
+  include Language::Python::Shebang
+
+  desc "Source code tag system"
+  homepage "https://www.gnu.org/software/global/"
+  url "https://ftp.gnu.org/gnu/global/global-6.6.7.tar.gz"
+  mirror "https://ftpmirror.gnu.org/global/global-6.6.7.tar.gz"
+  sha256 "69a0f77f53827c5568176c1d382166df361e74263a047f0b3058aa2f2ad58a3c"
+  license "GPL-3.0-or-later"
+
+  bottle do
+    sha256 arm64_big_sur: "ffba636507257bfb9dffd90dc114f1ad3528edf724beae0d200e3b5817f642bf"
+    sha256 big_sur:       "598cb4071a8d708c46becd27983443b6c14259c797d8a61718b0f1bb9094ae6f"
+    sha256 catalina:      "c476c6bee46e116a6dbe93754221132f8cc1cfbdb88d10c08d38b65c4e77b460"
+    sha256 mojave:        "37c6a139a405bbd869bcd8175e20a0e488ce0176646a0dbbfed3d9ede030977f"
+    sha256 x86_64_linux:  "13177da144e43810cbd458e90c24e160bf297b59db34208274973891f64da974"
+  end
+
+  head do
+    url ":pserver:anonymous:@cvs.savannah.gnu.org:/sources/global", using: :cvs
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "flex" => :build
+    ## gperf is provided by OSX Command Line Tools.
+    depends_on "libtool" => :build
+  end
+
+  depends_on "ctags"
+  depends_on "python@3.9"
+
+  uses_from_macos "ncurses"
+
+  on_linux do
+    depends_on "libtool"
+  end
+
+  skip_clean "lib/gtags"
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/ba/6e/7a7c13c21d8a4a7f82ccbfe257a045890d4dbf18c023f985f565f97393e3/Pygments-2.9.0.tar.gz"
+    sha256 "a18f47b506a429f6f4b9df81bb02beab9ca21d0a5fee38ed15aef65f0545519f"
+  end
+
+  def install
+    system "sh", "reconf.sh" if build.head?
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages("python3")
+
+    resource("Pygments").stage do
+      system "python3", *Language::Python.setup_install_args(libexec)
+    end
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --with-exuberant-ctags=#{Formula["ctags"].opt_bin}/ctags
+      --with-sqlite3
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    rewrite_shebang detected_python_shebang, share/"gtags/script/pygments_parser.py"
+
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+
+    etc.install "gtags.conf"
+
+    # we copy these in already
+    cd share/"gtags" do
+      rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      int c2func (void) { return 0; }
+      void cfunc (void) {int cvar = c2func(); }")
+    EOS
+    (testpath/"test.py").write <<~EOS
+      def py2func ():
+           return 0
+      def pyfunc ():
+           pyvar = py2func()
+    EOS
+
+    assert shell_output("#{bin}/gtags --gtagsconf=#{share}/gtags/gtags.conf --gtagslabel=pygments .")
+    assert_match "test.c", shell_output("#{bin}/global -d cfunc")
+    assert_match "test.c", shell_output("#{bin}/global -d c2func")
+    assert_match "test.c", shell_output("#{bin}/global -r c2func")
+    assert_match "test.py", shell_output("#{bin}/global -d pyfunc")
+    assert_match "test.py", shell_output("#{bin}/global -d py2func")
+    assert_match "test.py", shell_output("#{bin}/global -r py2func")
+    assert_match "test.c", shell_output("#{bin}/global -s cvar")
+    assert_match "test.py", shell_output("#{bin}/global -s pyvar")
+
+    assert shell_output("#{bin}/gtags --gtagsconf=#{share}/gtags/gtags.conf --gtagslabel=exuberant-ctags .")
+    # ctags only yields definitions
+    assert_match "test.c", shell_output("#{bin}/global -d cfunc   # passes")
+    assert_match "test.c", shell_output("#{bin}/global -d c2func  # passes")
+    assert_match "test.py", shell_output("#{bin}/global -d pyfunc  # passes")
+    assert_match "test.py", shell_output("#{bin}/global -d py2func # passes")
+    refute_match "test.c", shell_output("#{bin}/global -r c2func  # correctly fails")
+    refute_match "test.c", shell_output("#{bin}/global -s cvar    # correctly fails")
+    refute_match "test.py", shell_output("#{bin}/global -r py2func # correctly fails")
+    refute_match "test.py", shell_output("#{bin}/global -s pyvar   # correctly fails")
+
+    # Test the default parser
+    assert shell_output("#{bin}/gtags --gtagsconf=#{share}/gtags/gtags.conf --gtagslabel=default .")
+    assert_match "test.c", shell_output("#{bin}/global -d cfunc")
+    assert_match "test.c", shell_output("#{bin}/global -d c2func")
+    assert_match "test.c", shell_output("#{bin}/global -r c2func")
+    assert_match "test.c", shell_output("#{bin}/global -s cvar")
+  end
+end
